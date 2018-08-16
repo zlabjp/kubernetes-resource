889c9745b53e0262655400a4fe09ffaa773dd1b7
Kazuki Suda <ksuda@zlab.co.jp>
2018-08-16 17:57:41 +0900

    Add scripts/update-authors.sh that updates AUTHORS file

---
+++ scripts/verify-authors.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+SCRIPT_ROOT="$(dirname "${BASH_SOURCE}")/.."
+TMP_ROOT="$(mktemp -d)"
+
+cleanup() {
+  rm -rf "$TMP_ROOT"
+}
+trap "cleanup" EXIT SIGINT
+
+export AUTHORS_FILE="${TMP_ROOT}/AUTHORS"
+"${SCRIPT_ROOT}/scripts/update-authors.sh"
+
+ret=0
+diff "${SCRIPT_ROOT}/AUTHORS" "$AUTHORS_FILE" || ret=$?
+
+if [[ $ret -eq 0 ]]; then
+  echo "AUTHORS is up to date."
+else
+  echo "AUTHORS is out of date. Please run scripts/update-authors.sh."
+fi
